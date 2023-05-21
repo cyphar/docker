683dca40cdca6dc076af34f976f1cdd7e62d59f2
Aleksa Sarai <asarai@suse.de>
2023-05-21 12:53:42 +1000

    btrfs: do not error out based on LINUX_VERSION_CODE
    Linux distributions backport changes (such as trivial header changes
    referenced by the commit cf8c728f3c57b), and thus hard failing the build
    based on LINUX_VERSION_CODE makes it harder to package Docker for older
    distributions.
    
    The information in the warnings is useful, so switch to #warning.
    
    Fixes: cf8c728f3c57 ("graphdriver/btrfs: needs kernel headers >= 4.12, not >= 4.7")
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- daemon/graphdriver/btrfs/btrfs.go
+++ daemon/graphdriver/btrfs/btrfs.go
@@ -9,8 +9,8 @@ package btrfs // import "github.com/docker/docker/daemon/graphdriver/btrfs"
 
 #include <linux/version.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,12,0)
-    #error "Headers from kernel >= 4.12 are required to build with Btrfs support."
-    #error "HINT: Set 'DOCKER_BUILDTAGS=exclude_graphdriver_btrfs' to build without Btrfs."
+    #warning "Headers from kernel >= 4.12 are required to build with Btrfs support."
+    #warning "HINT: Set 'DOCKER_BUILDTAGS=exclude_graphdriver_btrfs' to build without Btrfs."
 #endif
 
 #include <linux/btrfs.h>
